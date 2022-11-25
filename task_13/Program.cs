5dfa500595d4941bb780f31ebfe2c83fec88563a
PhoenixMyron <phoenixmyron@gmail.com>
2022-11-25 14:34:20 +0600

    added third numder search

--- task_13/Program.cs
+++ task_13/Program.cs
@@ -1,1 +1,24 @@
 ﻿// Задача 13: Напишите программу, которая выводит третью цифру(c начала) заданного числа или сообщает, что третьей цифры нет.
+Console.Clear();
+
+ Console.Write("Введите число: ");
+ int n = Int32.Parse(Console.ReadLine());
+ int result = 0;
+ 
+ while (n > 0) 
+{
+     result *= 10;
+     result += n % 10;
+     n /= 10;
+}
+
+if ( result >= 100 )
+{
+    int n2 = result / 100;
+    int n3 = n2 % 10;
+    Console.Write($"{n3}" );
+}
+else 
+{
+    Console.Write("третьего числа нет");
+}
